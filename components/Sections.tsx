29612ae896e061570ce1cdba10da3cd141805629
SHO NAGASE <83164817+nagasho1122@users.noreply.github.com>
2022-11-28 10:11:26 +0900

    Dev 2743/create home mobile page (#1)
    * feat: create responsive css
    
    * feat: create mobile header
    
    * fix: header shadow
    
    * fix: add
    
    Co-authored-by: nagasho1122 <nmoca112222@gmail.com>

--- components/Sections.tsx
+++ components/Sections.tsx
@@ -1,23 +1,35 @@
 import Link from 'next/link'
+import { useState } from 'react'
 import styles from './sections.module.scss'
 
 export const Header = () => {
+  const [open, setOpen] = useState<Boolean>(false)
+
+  const changeHambugerState = () => {
+    setOpen((prevState) => !prevState)
+  }
+
   return (
     <header className={styles.header}>
-      <Link href="/somei_juku_sample" className={styles.link}>
-        <h1 className={styles.title_wrapper}>
-          <span className={styles.logo}>ロゴ</span>
-          <span className={styles.title}>予備校</span>
-        </h1>
-      </Link>
-      <nav>
-        <ul>
-          <li><Link href="/somei_juku_sample/about" className={styles.link}>ソメイ塾について</Link></li>
-          <li><Link href="/somei_juku_sample/curriculum" className={styles.link}>授業案内</Link></li>
-          <li><Link href="/somei_juku_sample/teacher" className={styles.link}>講師案内</Link></li>
-          <li><Link href="/somei_juku_sample/plan" className={styles.link}>料金プラン</Link></li>
-        </ul>
-      </nav>
+      <div className={styles.header_content_wrapper}>
+        <Link href="/somei_juku_sample" className={styles.link}>
+          <h1 className={styles.title_wrapper}>
+            <span className={styles.logo}>ロゴ</span>
+            <span className={styles.title}>予備校</span>
+          </h1>
+        </Link>
+        <div className={styles.hamburger_menu} onClick={changeHambugerState}>
+          <span className={open ? styles.clicked : ''} />
+        </div>
+        <nav className={open ? styles.nav_opened : styles.nav_closed}>
+          <ul>
+            <li><Link href="/somei_juku_sample/about" onClick={changeHambugerState} className={styles.nav_link}>ソメイ塾について</Link></li>
+            <li><Link href="/somei_juku_sample/curriculum" onClick={changeHambugerState} className={styles.nav_link}>授業案内</Link></li>
+            <li><Link href="/somei_juku_sample/teacher" onClick={changeHambugerState} className={styles.nav_link}>講師案内</Link></li>
+            <li><Link href="/somei_juku_sample/plan" onClick={changeHambugerState} className={styles.nav_link}>料金プラン</Link></li>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
