a1cff5c50f84c842ab4e38914a43fd315187b4a9
nagasho1122 <nmoca112222@gmail.com>
2022-11-28 10:06:30 +0900

    feat: create title image comp

---
+++ components/TitleImage.tsx
@@ -0,0 +1,14 @@
+import styles from './sections.module.scss'
+
+
+export const TitleImage = ({
+  title,
+  imageSrc,
+} : {
+  title: string,
+  imageSrc: string
+}) => {
+  return (
+    <></>
+  )
+}
