4791bf3a0c34d05753e517053e8660fd1510eca6
nagasho1122 <nmoca112222@gmail.com>
2022-12-01 22:20:46 +0900

    feat: create cs tag

---
+++ components/CvTag.tsx
@@ -0,0 +1,33 @@
+import styles from './cvTag.module.scss'
+import Link from "next/link"
+import { useRouter } from 'next/router'
+
+export const CvTag = ({
+  link,
+  content
+} : {
+  link: string,
+  content: string
+}) => {
+  const currentUrl = useRouter().pathname
+  console.log(currentUrl)
+  console.log(link)
+
+  const cvTagObject = <div className={styles.cv_tag_wrapper}>
+                        <Link className={styles.cv_tag_link} href={link}>
+                          <div className={styles.cv_tag_top_box}>&gt;</div>
+                          <div className={styles.cv_tag_text_wrapper}>
+                            <p className={styles.cv_tag_text}>{content}</p>
+                          </div>
+                        </Link>
+                      </div>
+  return (
+    <>
+      {
+        currentUrl === link
+          ? null
+          : cvTagObject
+      }
+    </>
+  )
+}
--- components/Layouts.tsx
+++ components/Layouts.tsx
@@ -1,10 +1,14 @@
 import { Footer, Header } from './Sections'
 import styles from './layouts.module.scss'
+import { CvTag } from './Cvtag'
 
 const Layout = ({children} : {children: JSX.Element}) => {
   return (
     <div className={styles.wrapper}>
       <Header />
+      <div className={styles.cv_tag_wrapper}>
+        <CvTag link="/somei_juku_sample/about" content="資料請求・体験授業のご予約受付中" />
+      </div>
       { children }
       <Footer />
     </div>
